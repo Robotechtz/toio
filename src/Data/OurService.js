f04dcc6289bef7035511ab37efb2cad1fecc4971
Hatibu <juliushatibu@gmail.com>
2022-01-05 12:46:54 +0300

    feat:added Data folder

---
+++ src/Data/OurService.js
@@ -0,0 +1,40 @@
+import React from 'react';
+export const OurService = [
+    {   
+        id: 0,
+        img:'https://firebasestorage.googleapis.com/v0/b/medias-da388.appspot.com/o/toio-media%2FTraining-07.png?alt=media&token=56d0ade9-9c19-4eb5-bff4-25c395a9c401',
+        head:'Training Programs',
+        title: 'A wide range of educational courses in areas of Robotics, Software Development, AI, IOT and more',
+        
+      },
+
+      {   
+        id: 1,
+        img:'https://firebasestorage.googleapis.com/v0/b/medias-da388.appspot.com/o/toio-media%2Fprototyping-07.png?alt=media&token=ce058c72-1bb5-4507-9e59-8e48078f641c',
+        head:'Prototyping And Testing',
+        title: 'Taking the vision from an idea into reality with the first steps towards the first prototyp',
+        
+      },
+
+      {   
+        id: 2,
+        img:'https://firebasestorage.googleapis.com/v0/b/medias-da388.appspot.com/o/icon3.png?alt=media&token=653cdca0-f423-4d0f-9be5-ac6a603ba71b',
+        head:'Fabrication And Design',
+        title: 'Advancement of the prototype/ vision towards a more prominent feel and look',
+     
+        
+      },
+
+      {   
+        id: 3,
+        img:'https://firebasestorage.googleapis.com/v0/b/medias-da388.appspot.com/o/toio-media%2Ftechnical-07.png?alt=media&token=82be6d04-63cb-4003-b80e-05fd27afb133',
+        head:'Technical Consultancy And Support',
+        title: 'Assistance and guidance in various areas of interest across various tech spaces',
+     
+        
+      },
+
+      
+      
+
+]
