85536b0b9182339e796e875d338d88e04c79f29e
Hatibu <juliushatibu@gmail.com>
2022-01-07 11:32:24 +0300

    feat:added team json data

---
+++ src/Data/Team.js
@@ -0,0 +1,49 @@
+import React from 'react';
+export const Team = [
+    {   
+        id: 0,
+        img:'https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/shaukat.png?alt=media&token=3f9cb6df-0b10-490e-b7ed-9011b187c692',
+        title:'Technical Director',
+        name: 'Shaukatali Hussein',
+        
+      },
+
+      {   
+        id: 1,
+        img:'https://firebasestorage.googleapis.com/v0/b/ecommerce-2050.appspot.com/o/stella.jpg?alt=media&token=85c9793a-5485-47cf-b9dd-2c25db94a5d7',
+        title:'Telecom Engineer',
+        name: 'Stella Mwinuka',
+        
+      },
+
+      {   
+        id: 2,
+        img:'https://firebasestorage.googleapis.com/v0/b/ecommerce-2050.appspot.com/o/peter.jpg?alt=media&token=023c3e35-0336-42e4-ab10-4a461a7a64b9',
+        title:'Mechatronics Engineer',
+        name: 'Peter Mtimavalye',
+     
+        
+      },
+
+      {   
+        id: 3,
+        img:'https://firebasestorage.googleapis.com/v0/b/ecommerce-2050.appspot.com/o/sule.jpg?alt=media&token=60594fb2-c85a-4dc6-ba80-c3ff47d632ab',
+        title:'Software Engineer',
+        name: 'Suleiman Msumari',
+     
+        
+      },
+      {   
+        id: 4,
+        img:'https://firebasestorage.googleapis.com/v0/b/ecommerce-2050.appspot.com/o/julius.jpg?alt=media&token=bbf6a407-02f8-4eb4-a2ee-96c867053913',
+        title:'Software Engineer',
+        name: 'Julius Hatibu',
+     
+        
+      },
+
+      
+      
+
+]
+export default Team
