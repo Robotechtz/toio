afb727fa54d3992bc7a10aa8b6b001667cd33af3
msumari <suleykhery@gmail.com>
2022-03-10 10:16:38 +0300

    fix:removed Peter from the team

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,106 +1,85 @@
-
 export const Team = [
-    {   
-        id: 0,
-        img:'https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/shaukat.png?alt=media&token=3f9cb6df-0b10-490e-b7ed-9011b187c692',
-        title:'Technical Director',
-        name: 'Shaukatali Hussein',
-        
-      },
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/shaukat.png?alt=media&token=3f9cb6df-0b10-490e-b7ed-9011b187c692",
+    title: "Technical Director",
+    name: "Shaukatali Hussein",
+  },
 
-      {   
-        id: 1,
-        img:'https://firebasestorage.googleapis.com/v0/b/ecommerce-2050.appspot.com/o/stella.jpg?alt=media&token=85c9793a-5485-47cf-b9dd-2c25db94a5d7',
-        title:'Telecom Engineer',
-        name: 'Stella Mwinuka',
-        
-      },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/ecommerce-2050.appspot.com/o/stella.jpg?alt=media&token=85c9793a-5485-47cf-b9dd-2c25db94a5d7",
+    title: "Telecom Engineer",
+    name: "Stella Mwinuka",
+  },
 
-      {   
-        id: 2,
-        img:'https://firebasestorage.googleapis.com/v0/b/ecommerce-2050.appspot.com/o/peter.jpg?alt=media&token=023c3e35-0336-42e4-ab10-4a461a7a64b9',
-        title:'Mechatronics Engineer',
-        name: 'Peter Mtimavalye',
-     
-        
-      },
-
-      {   
-        id: 3,
-        img:'https://firebasestorage.googleapis.com/v0/b/ecommerce-2050.appspot.com/o/sule.jpg?alt=media&token=60594fb2-c85a-4dc6-ba80-c3ff47d632ab',
-        title:'Software Engineer',
-        name: 'Suleiman Msumari',
-     
-        
-      },
-      {   
-        id: 4,
-        img:'https://firebasestorage.googleapis.com/v0/b/ecommerce-2050.appspot.com/o/julius.jpg?alt=media&token=bbf6a407-02f8-4eb4-a2ee-96c867053913',
-        title:'Software Engineer',
-        name: 'Julius Hatibu',
-     
-        
-      },
-
-      
-      
-
-]
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/ecommerce-2050.appspot.com/o/sule.jpg?alt=media&token=60594fb2-c85a-4dc6-ba80-c3ff47d632ab",
+    title: "Software Engineer",
+    name: "Suleiman Msumari",
+  },
+  {
+    id: 3,
+    img: "https://firebasestorage.googleapis.com/v0/b/ecommerce-2050.appspot.com/o/julius.jpg?alt=media&token=bbf6a407-02f8-4eb4-a2ee-96c867053913",
+    title: "Software Engineer",
+    name: "Julius Hatibu",
+  },
+];
 
 export const Exproledata = [
-    {   
-        id: 0,
-        title:'Innovation',
-        desc: 'Supporting local innovators with relevant skills and programs, accessibility of tools and technical support towards innovation and impact',
-        pic:"https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/page-03.png?alt=media&token=9dc821e6-2421-41c3-ba94-9d0a5c6a31c4"
-      },
-
-      {   
-        id: 1,
-        title:'Ecosystem',
-        desc: 'Creating a conducive ecosystem around the community where ideas are brought to reality',
-        pic:"https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/page-01.png?alt=media&token=07a78248-8fc0-4465-a10f-a17a0b957fcd"
-      },
-      
-
-      {   
-        id: 2,
-        title:'Foundation',
-        desc: 'Scalable and sustainable CO-OP’s that are managed and lead by our very own makers',
-        pic:"https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/page-02.png?alt=media&token=1cbb88cd-bad9-4783-9fc0-1d0a3ec38aad"
-      },
-
-      {   
-        id: 3,
-        title:'Growth',
-        desc: 'Community of evolving innovators and makers supporting one another in various aspects of innovations',
-        pic:"https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/page-04.png?alt=media&token=255884f7-f017-401d-98da-9e4558457198"
-      },
-    ]
-
-export const Servicedata=[
   {
-    id:0,
-    desc:"A wide range of educational courses in areas of Robotics,Software Development,AI, IOT and more",
-    title:"Training Programs",
-    pic:"https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/Training-07.png?alt=media&token=56ceafa8-59c1-4bb0-ba99-4e85e43e316e",
+    id: 0,
+    title: "Innovation",
+    desc: "Supporting local innovators with relevant skills and programs, accessibility of tools and technical support towards innovation and impact",
+    pic: "https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/page-03.png?alt=media&token=9dc821e6-2421-41c3-ba94-9d0a5c6a31c4",
+  },
+
+  {
+    id: 1,
+    title: "Ecosystem",
+    desc: "Creating a conducive ecosystem around the community where ideas are brought to reality",
+    pic: "https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/page-01.png?alt=media&token=07a78248-8fc0-4465-a10f-a17a0b957fcd",
+  },
+
+  {
+    id: 2,
+    title: "Foundation",
+    desc: "Scalable and sustainable CO-OP’s that are managed and lead by our very own makers",
+    pic: "https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/page-02.png?alt=media&token=1cbb88cd-bad9-4783-9fc0-1d0a3ec38aad",
+  },
+
+  {
+    id: 3,
+    title: "Growth",
+    desc: "Community of evolving innovators and makers supporting one another in various aspects of innovations",
+    pic: "https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/page-04.png?alt=media&token=255884f7-f017-401d-98da-9e4558457198",
+  },
+];
+
+export const Servicedata = [
+  {
+    id: 0,
+    desc: "A wide range of educational courses in areas of Robotics,Software Development,AI, IOT and more",
+    title: "Training Programs",
+    pic: "https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/Training-07.png?alt=media&token=56ceafa8-59c1-4bb0-ba99-4e85e43e316e",
   },
   {
-    id:1,
-    desc:"Taking the vision from an idea into reality with the first steps towards the first prototype",
-    title:"Prototyping And Testing",
-    pic:"https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/prototyping-07.png?alt=media&token=772bd2f1-493b-4263-b1d0-f832cbf31f1d",
+    id: 1,
+    desc: "Taking the vision from an idea into reality with the first steps towards the first prototype",
+    title: "Prototyping And Testing",
+    pic: "https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/prototyping-07.png?alt=media&token=772bd2f1-493b-4263-b1d0-f832cbf31f1d",
   },
   {
-    id:2,
-    desc:"Advancement of the prototype vision towards a more prominent feel and look",
-    title:"Fabrication And Design",
-    pic:"https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/Fabrication-07.png?alt=media&token=c4921f18-5b08-4491-a354-640022f77dd6",
+    id: 2,
+    desc: "Advancement of the prototype vision towards a more prominent feel and look",
+    title: "Fabrication And Design",
+    pic: "https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/Fabrication-07.png?alt=media&token=c4921f18-5b08-4491-a354-640022f77dd6",
   },
   {
-    id:3,
-    desc:"Assistance and guidance in various areas of interest across various tech spaces",
-    title:"Tech. Consultancy And Support",
-    pic:"https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/technical-07.png?alt=media&token=9d8356f0-8937-456a-a95f-c4cb93652e78",
+    id: 3,
+    desc: "Assistance and guidance in various areas of interest across various tech spaces",
+    title: "Tech. Consultancy And Support",
+    pic: "https://firebasestorage.googleapis.com/v0/b/robotech-web.appspot.com/o/technical-07.png?alt=media&token=9d8356f0-8937-456a-a95f-c4cb93652e78",
   },
-]
+];
--- src/components/OurTeam.js
+++ src/components/OurTeam.js
@@ -1,43 +1,38 @@
-import React from 'react';
-import {Team} from '../Data/data';
+import React from "react";
+import { Team } from "../Data/data";
 import "./styles/team.css";
 
-
-
 function OurTeam() {
-    return (
-      
-        <div className="h-screen overflow-auto mt-48  lg:mt-0 ">
-        <div className="bg-white  w-full">
+  return (
+    <div className="h-screen overflow-auto mt-48  lg:mt-0 ">
+      <div className="bg-white  w-full">
         <div className="w-full grid place-items-center lg:py-10">
-            <h1 className="text-xl md:text-4xl lg:text-4xl font-bold">meet our <span className="text-red-600">TEAM</span></h1>
-          </div>
-        <div className="grid lg:grid-cols-5  ">
-         {Team.map((item, index) => {
-           return(
-            <div key={index} className=" h-full rounded-lg pl-7 ">
-              <div className="bg-white w-3/4 ">
-              <img
-                src={item.img}
-                alt="engineering"
-                className="object-cover h-full lg:h-60 rounded-lg"
-              />
-              <p className="text-lg text-black text-center font-bold">
-                {item.title}
-              </p>
-              <p className="text-black mb-6 text-center">
-                {item.name}
-              </p>
+          <h1 className="text-xl md:text-4xl lg:text-4xl font-bold">
+            meet our <span className="text-red-600">TEAM</span>
+          </h1>
+        </div>
+        <div className="grid lg:grid-cols-4  ">
+          {Team.map((item, index) => {
+            return (
+              <div key={index} className=" h-full rounded-lg pl-7 ">
+                <div className="bg-white w-3/4 ">
+                  <img
+                    src={item.img}
+                    alt="engineering"
+                    className="object-cover h-full lg:h-60 rounded-lg"
+                  />
+                  <p className="text-lg text-black text-center font-bold">
+                    {item.title}
+                  </p>
+                  <p className="text-black mb-6 text-center">{item.name}</p>
+                </div>
               </div>
-            </div>
-          
-           )
-         })}
-         </div>
-         </div>
-         </div>
-      
-    )
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default OurTeam
+export default OurTeam;
